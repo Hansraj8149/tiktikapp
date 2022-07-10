30588cb1eb1f82df63d28f6d61cbd9481d1c22a6
Hansraj8149 <hansrajsaini8149@gmail.com>
2022-07-11 01:06:42 +0530

    added ignoreBuildErrors in nextConfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ['img.freepik.com', 'lh3.googleusercontent.com', 'external-preview.redd.it', 'yt3.ggpht.com', 'd.wattpad.com', 'preview.redd.it', 'th.bing.com', 'wallpapercave.com']
