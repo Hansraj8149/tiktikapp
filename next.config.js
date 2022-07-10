b6efcea314e1e0622f6be61f16b9577086d05762
Hansraj8149 <hansrajsaini8149@gmail.com>
2022-07-11 00:27:21 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['img.freepik.com', 'lh3.googleusercontent.com', 'external-preview.redd.it', 'yt3.ggpht.com', 'd.wattpad.com', 'preview.redd.it', 'th.bing.com', 'wallpapercave.com']
+  }
+}
+
+module.exports = nextConfig
